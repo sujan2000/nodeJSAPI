041a31e174627de6df885ec0f9fe69adce445fe2
sujan2000 <meena.sujan06@gmail.com>
2026-01-07 22:53:33 +0530

    change to load

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import http from 'node:http'
+// import path from 'node:path'
+// import { dirname } from 'node:path'
+// import { fileURLToPath } from 'node:url'
 import { serveStatic } from './utils/serveStatic.js'
 import { handleGet } from './handlers/routeHandlers.js'
 import { handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 8000
+
 const __dirname = import.meta.dirname
-console.log(__dirname)
+
+// const __filename = fileURLToPath(import.meta.url)
+// console.log(__dirname)
+// console.log("CWD", process.cwd())
+
+// const pathToResource = path.join(__dirname, 'public', 'index.html')
+// console.log(pathToResource)
 
 const server = http.createServer(async (req, res) => {
-
     if (req.url === '/api') {
         if (req.method === 'GET') {
             return await handleGet(res)
