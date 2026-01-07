b7510a30aef887ddb6a804eae6a5a5091d6576f3
sujan2000 <meena.sujan06@gmail.com>
2026-01-07 23:44:17 +0530

    Add Vercel KV for persistent data storage

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import { kv } from '@vercel/kv'
 import { getData } from '../utils/getData.js'
 import { addNewSighting } from '../utils/addNewSighting.js'
 import { sanitizeInput } from '../utils/sanitizeInput.js'
@@ -5,7 +6,11 @@ import { sanitizeInput } from '../utils/sanitizeInput.js'
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const data = await getData()
+      let data = await kv.get('sightings')
+      if (!data) {
+        data = await getData()
+        await kv.set('sightings', data)
+      }
       res.status(200).json(data)
     } catch (err) {
       res.status(500).json({ error: err.message })
@@ -14,8 +19,12 @@ export default async function handler(req, res) {
     try {
       const parsedBody = req.body
       const sanitizedBody = sanitizeInput(parsedBody)
-      // Note: In Vercel serverless, file system is read-only, so we can't save to file
-      // For production, use a database
+      let data = await kv.get('sightings')
+      if (!data) {
+        data = await getData()
+      }
+      data.push(sanitizedBody)
+      await kv.set('sightings', data)
       res.status(201).json(sanitizedBody)
     } catch (err) {
       res.status(400).json({ error: err.message })
