cd033df08c6ed051db57cf63c7d4768ced38bafa
sujan2000 <meena.sujan06@gmail.com>
2026-01-07 20:02:42 +0530

    add env and gitignore

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import { handleGet } from './handlers/routeHandlers.js'
 import { handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 8000
-
 const __dirname = import.meta.dirname
+console.log(__dirname)
 
 const server = http.createServer(async (req, res) => {
 
