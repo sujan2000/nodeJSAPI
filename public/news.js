90c25ea7818833df0d7e3e2f74fb867177a27abe
sujan2000 <meena.sujan06@gmail.com>
2026-01-08 20:51:08 +0530

    update

--- public/news.js
+++ public/news.js
@@ -1,2 +1,13 @@
 const eventSource = new EventSource("http://localhost:8000/api/news")
+
 const liveContainer = document.getElementById("live-container")
+
+eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data)
+    const story = data.story
+    liveContainer.textContent = story
+}
+
+eventSource.onerror = () => {
+    console.log("Connection lost. Attempting to reconnect...")
+}
